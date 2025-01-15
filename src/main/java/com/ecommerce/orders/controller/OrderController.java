6af35327a3f81286cbf52da4bba930e374c23e39
shareequa-bit <shareequa6@gmail.com>
2025-01-16 01:49:09 +0530

    to test app in ec2

--- src/main/java/com/ecommerce/orders/controller/OrderController.java
+++ src/main/java/com/ecommerce/orders/controller/OrderController.java
@@ -1,5 +1,9 @@
 package com.ecommerce.orders.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +24,19 @@ public class OrderController {
 	
 	@GetMapping("/getOrder/{id}")
 	public EcommerceOrder getOrder(@PathVariable("id") Integer id) {
-		System.out.println("getting an order of id" +id);
+		//System.out.println("getting an order of id " +id);
 		//return new EcommerceOrder(101, "Order1111","shareequa", 2000.0, 2);
 		return orderService.getOrder(id);
+	}
+	
+	@GetMapping("")
+	public List<EcommerceOrder> getOrders() {
+	   
+		List<EcommerceOrder> list=new ArrayList<>();
+		list.add(new EcommerceOrder( 101, "Order1111","shareequa", 2000.0, 2));
+		list.add(new EcommerceOrder( 102, "Order1112","shareequa", 1000.0, 1));
+		list.add(new EcommerceOrder( 103, "Order1113","shareequa", 5000.0, 3));
+		return list;
 	}
 
 }
